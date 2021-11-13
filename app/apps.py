48bf4fcb2c28f4219959be9753fba370f84fc045
rafalsiniewicz <36672426+rafalsiniewicz@users.noreply.github.com>
2021-11-13 21:02:22 +0100

    dont run cpp server at collectstatic

--- app/apps.py
+++ app/apps.py
@@ -5,6 +5,7 @@ import time
 from multiprocessing import Process
 import subprocess
 from subprocess import Popen
+import sys
 
 def run_server():
 	subprocess.run('./server', shell=True)
@@ -18,18 +19,20 @@ class AppConfig(AppConfig):
         self.trie = Trie()  
 
     def ready(self) -> None:
-        # pass
-        proc = Popen('chmod +x server', shell=True,
-             stdin=None, stdout=None, stderr=None, close_fds=True)
-        proc2 = Popen('./server', shell=True,
-             stdin=None, stdout=None, stderr=None, close_fds=True)
-        # p = Process(target=run_server)
-        # p.start()
-        # print("here")
-        # p.join()
-        # os.system('  ')
-        # os.system('./server')
-        # pass
+        print("argv ", sys.argv)
+        if 'runserver' in sys.argv:
+            # pass
+            proc = Popen('chmod +x server', shell=True,
+                stdin=None, stdout=None, stderr=None, close_fds=True)
+            proc2 = Popen('./server', shell=True,
+                stdin=None, stdout=None, stderr=None, close_fds=True)
+            # p = Process(target=run_server)
+            # p.start()
+            # print("here")
+            # p.join()
+            # os.system('  ')
+            # os.system('./server')
+            # pass
         # start = time.time()
         # with open(os.path.join("sjp-20210625","short_words_11.txt"), 'r', encoding='utf-8') as fp:
         #     for line in fp:
